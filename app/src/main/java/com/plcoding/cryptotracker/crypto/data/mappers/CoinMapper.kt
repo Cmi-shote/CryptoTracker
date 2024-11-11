ea6871d892f8e05ff07fa03ca038ab4c585d2f41
similoluwashote <simi.shote@babbangona.com>
2024-11-11 19:08:27 +0100

    - fetch coin history

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -1,7 +1,11 @@
 package com.plcoding.cryptotracker.crypto.data.mappers
 
 import com.plcoding.cryptotracker.crypto.data.networking.dataTransferObject.CoinDto
+import com.plcoding.cryptotracker.crypto.data.networking.dataTransferObject.CoinPriceDto
 import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
+import java.time.Instant
+import java.time.ZoneId
 
 fun CoinDto.toCoin(): Coin {
     return Coin(
@@ -12,5 +16,13 @@ fun CoinDto.toCoin(): Coin {
         marketCapUsd = marketCapUsd,
         priceUsd = priceUsd,
         changePricePercent24Hr = changePercent24Hr
+    )
+}
+
+fun CoinPriceDto.toCoinPrice(): CoinPrice {
+    return CoinPrice(
+        priceUsd = priceUsd,
+        dateTime = Instant.ofEpochMilli(time)
+            .atZone(ZoneId.of("UTC"))
     )
 }
--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,23 +6,55 @@ import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
 import com.plcoding.cryptotracker.core.domain.util.map
 import com.plcoding.cryptotracker.crypto.data.mappers.toCoin
+import com.plcoding.cryptotracker.crypto.data.networking.dataTransferObject.CoinHistoryDto
 import com.plcoding.cryptotracker.crypto.data.networking.dataTransferObject.CoinsResponseDto
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 //load data source using ktor
+// CoinCapAPI: https://docs.coincap.io/
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
 ) : CoinDataSource{
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
         return safeCall<CoinsResponseDto> {
             httpClient.get(constructUrl("/assets"))
-        }.map {
-            response ->
+        }.map { response ->
             response.data.map { it.toCoin() }
         }
     }
 
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+
+        val endMillis = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+
+        return safeCall<CoinHistoryDto {
+            httpClient.get(
+                constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6") //get price changed in 6hrs
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
+        }
+    }
 }
